6e0e23e795fbf459f79fc6742227ca80233694b6
va3dxv <45865432+va3dxv@users.noreply.github.com>
2019-01-26 14:50:14 -0500

    Update button.py

--- button.py
+++ button.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 all_relays = [11, 12, 13, 14, 15, 16, 17, 18]
+num_relays = len(all_relays)
+
 relay1 = 11
 relay2 = 12
 relay3 = 13
@@ -11,50 +13,53 @@ relay5 = 15
 relay6 = 16
 relay7 = 17
 relay8 = 18
-button_up = 21
-button_dn = 26
+button1 = 21
+button8 = 26
 
 def setup():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(button_up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(button_dn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(button8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(all_relays, GPIO.OUT, initial=GPIO.HIGH)
 
-def up(channel):
-    print("Up button pressed.")
-    if GPIO.input(18):
-        print("Relay 8 was OFF. Switching ON.")
-        GPIO.output(relay8, GPIO.LOW)
-    else:
-        print("Relay 8 was ON. Switching OFF.")
-        GPIO.output(relay8, GPIO.HIGH)
+def one(channel):
+    if GPIO.input(relay1):
 
-def down(channel):
-    print("Down button pressed.")
-    if GPIO.input(11):
         print("Relay 1 was OFF. Switching ON.")
+        GPIO.output(all_relays, GPIO.HIGH)
         GPIO.output(relay1, GPIO.LOW)
+
     else:
         print("Relay 1 was ON. Switching OFF.")
-        GPIO.output(relay1, GPIO.HIGH)
+        GPIO.output(all_relays, GPIO.HIGH)
+
+def eight(channel):
+    if GPIO.input(relay8):
+
+        print("Relay 8 was OFF. Switching ON.")
+        GPIO.output(all_relays, GPIO.HIGH)
+        GPIO.output(relay8, GPIO.LOW)
+
+    else:
+        print("Relay 8 was ON. Switching OFF.")
+        GPIO.output(all_relays, GPIO.HIGH)
 
 def main():
-    GPIO.add_event_detect(button_up, GPIO.FALLING, callback=up)
-    GPIO.add_event_detect(button_dn, GPIO.FALLING, callback=down)
+    GPIO.add_event_detect(button1, GPIO.FALLING, callback=one, bouncetime=300)
+    GPIO.add_event_detect(button8, GPIO.FALLING, callback=eight, bouncetime=300)
 
 def destroy():
     GPIO.output(all_relays, GPIO.HIGH)
+    sleep(0.5)
     GPIO.cleanup()
     print('\nQuitting.\n')
 
 if __name__ == '__main__':
     setup()
-
     try:
         main()
         while(True):
-            time.sleep(0.1)
-
+            sleep(0.5)
     except KeyboardInterrupt:
         destroy()
